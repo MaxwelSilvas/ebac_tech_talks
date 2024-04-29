34a8dfea9304194f5e38da7cc69d8442aca8c9a4
Maxwel Silva <108342352+MaxwelSilvas@users.noreply.github.com>
2024-04-29 13:24:44 -0300

    Update main.js
    retirando o console.log do codigo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,11 +24,6 @@ const contaAsHoras = setInterval(function () {
     (distanciaAteOEvento % minutoEmMs) / 1000
   );
 
-  console.log(diasAteOEvento);
-  console.log(horasAteOEvento);
-  console.log(minutosAteOEvento);
-  console.log(segundosAteOEvento);
-
   document.getElementById(
     "contador"
   ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
